c9acaf71887f3bdd808dd45651aaa23dc617ee92
kompiuter <kyriacos.s.kyriacou@gmail.com>
2019-04-03 01:23:44 +0300

    Add closing

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println(Greeting())
+	fmt.Println(Closing())
 }
 
 func Greeting() string {
 	return "hello world!"
+}
+
+func Closing() string {
+	return "goodbye!"
 }
--- main_test.go
+++ main_test.go
@@ -4,9 +4,24 @@ import (
 	"fmt"
 )
 
+func ExampleMain() {
+	main()
+
+	// Output:
+	// hello world!
+	// goodbye!
+}
+
 func ExampleGreeting() {
 	fmt.Println(Greeting())
 
 	// Output:
 	// hello world!
+}
+
+func ExampleClosing() {
+	fmt.Println(Closing())
+
+	// Output:
+	// goodbye!
 }
